0d74e7e0a757fdab0237aa0145129579f502f88a
Oleg Maizer <zippooleg3@gmail.com>
2022-11-27 02:28:12 +0200

    Added hero video

---
+++ src/components/HeroVid.jsx
@@ -0,0 +1,12 @@
+
+const HeroVid = () => {
+  return (
+    <div>
+        <video loop autoPlay muted>
+            <source src="img/hero.mp4" type="video/mp4"/>
+        </video>
+    </div>
+  )
+}
+
+export default HeroVid
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HeroVid from '../../components/HeroVid'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <HeroVid/>
+    </div>
   )
 }
 
